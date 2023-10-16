7be00f507fc4fc620c26af418a027f67ca624b93
muneebullah <muneebullahmuneeb123@gmail.com>
2023-10-16 09:43:45 +0430

    Adding project to github

--- src/main/java/com/forTestOnly/Main.java
+++ src/main/java/com/forTestOnly/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Pushing project from intelliJ IDEA to github!");
+        System.out.println("This is for only practice purpose");
         }
     }
